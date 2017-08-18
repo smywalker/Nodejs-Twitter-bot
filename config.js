e1c27a14ef38692a36daee1beed75dfd4cd1990b
Luke Smy <smywalker@users.noreply.github.com>
2017-08-19 00:56:05 +0100

    changed/secured config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //Twitter Oauth info object.
 module.exports = {
-    consumer_key:         'TKlktuK6YH46lfLZVV4vM5JEW',
-    consumer_secret:      'ZnC0Ru8HKIU32aK3ZliY1IPhJpUGQixn33Yg8jsPfQFAJEDuu9',
+    consumer_key:         'tKlktuK6YH46lfLZVV4vM5JEW',
+    consumer_secret:      'znC0Ru8HKIU32aK3ZliY1IPhJpUGQixn33Yg8jsPfQFAJEDuu9',
     access_token:         '897490414432923648-UMeyAjuigi1LNt3NwPZVnPs5d2mmfaT',
-    access_token_secret:  'kiWOI3Vp7X5lyIDoPafB2D4Lxxi1wdywOd3vbWhQF8aK6'
+    access_token_secret:  'KiWOI3Vp7X5lyIDoPafB2D4Lxxi1wdywOd3vbWhQF8aK6'
   }
