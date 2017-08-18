98920ef8e70f561cf4159e7255a4ed2907bcd55e
smyl <smyl@coventry.ac.uk>
2017-08-19 00:50:19 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+//Twitter Oauth info object.
+module.exports = {
+    consumer_key:         'TKlktuK6YH46lfLZVV4vM5JEW',
+    consumer_secret:      'ZnC0Ru8HKIU32aK3ZliY1IPhJpUGQixn33Yg8jsPfQFAJEDuu9',
+    access_token:         '897490414432923648-UMeyAjuigi1LNt3NwPZVnPs5d2mmfaT',
+    access_token_secret:  'kiWOI3Vp7X5lyIDoPafB2D4Lxxi1wdywOd3vbWhQF8aK6'
+  }
---
+++ index.js
@@ -0,0 +1,45 @@
+//A twitter bot that tells Zlatan Ibrahimovic quotes jokes.
+var request = require('request');
+var twit = require('twit');
+var config = require('./config');
+var T = new twit(config);
+
+getData();
+setInterval(getData, 1000*60);
+
+//Get the joke.
+function getData(){
+    request({
+        url:'https://api.chucknorris.io/jokes/random',
+        json: true
+    },Joke);
+};
+//callback function from request(when the data has been collected)
+function Joke(error, response, body){
+    var joke = body.value;
+    var firstNames = ['Chuck','chuck','chuck.','Chuck.','Chuck!','chuck!'];
+    var lastNames = ['Norris','norris','norris.','Norris.','Norris!','norris!'];  
+    var altOne = wordInString(joke, firstNames,'Zlatan');
+    var altTwo = wordInString(altOne, lastNames,'Ibrahimovic');
+    var altThree = wordInString(altTwo,["Chucks's","chucks's"],"Zlatans's");
+    var altFour = wordInString(altThree,["Norris'","norris's"],"Ibrahimovic's");
+    if (error) {
+        console.log("There was an Error: "+ error);
+    }
+    postTweet(altFour);
+};
+//tweet the joke.
+function postTweet(txt){
+    var tweet = { status:txt};
+    T.post('statuses/update', tweet, (error, data, response)=>{
+        if(error){
+            console.log("There was an Error: "+ error);
+        }
+    });
+}
+//replace characters
+function wordInString(str, words, replacement){ 
+    var rex = new RegExp( '\\b' + words.join('|') + '\\b','gi');
+    return str.replace(rex, replacement);
+}
+
